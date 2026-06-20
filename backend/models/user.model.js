905aaff2df2bf4736f6c6a3d07bf8066f62ec821
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-20 15:01:10 +0530

    job model created

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,47 +1,47 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true,
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['student', 'recruiter'],
+    required: true,
+  },
+  profile: {
+    bio: {
+      type: String,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+    skills: [{ type: String }],
+    resume: {
+      type: String   // url to resume file
     },
-    phoneNumber: {
-        type: Number,
-        required: true,
+    resumeOriginalName: {
+      type: String,
     },
-    password:{
-      type:String,
-      required:true,
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
     },
-    role :{
-        type:String,
-        enum:['student', 'recruiter'],
-        required:true,
-    },
-    profile:{
-        bio:{
-            type:String,
-        },
-        skills:[{type:String}],
-      resume:{
-        type:String   // url to resume file
-      },
-      resumeOriginalName:{
-        type:String,
-      },
-      company :{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Company',
-      },
-      profilePhoto:{
-        type:String,
-        default:"",
-      }
-    },
-},{timestamps:true});
-export const User=mongoose.model('User',userSchema);
+    profilePhoto: {
+      type: String,
+      default: "",
+    }
+  },
+}, { timestamps: true });
+export const User = mongoose.model('User', userSchema);
