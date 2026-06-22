713afed9fe754a30f410e5c105fcd804abc4346b
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-22 15:28:22 +0530

    testing

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,5 +1,5 @@
 import { Company } from "../models/company.model.js"
-export const registerCompan = async (req, res) => {
+export const registerCompany = async (req, res) => {
     try {
         const { companyName } = req.body;
         if (!companyName) {
@@ -18,7 +18,7 @@ export const registerCompan = async (req, res) => {
         };
         company = await Company.create({
             name: companyName,
-            userId=req.id
+            userId:req.id
         })
 
         return res.status(201).json({
@@ -33,7 +33,7 @@ export const registerCompan = async (req, res) => {
 
 export const getCompany = async (req, res) => {
     try {
-        const usedId = req.id;   // logged in user id
+        const userId = req.id;   // logged in user id
         const companies = await Company.find({ userId });
         if (!companies) {
             return res.status(404).json({
@@ -41,6 +41,11 @@ export const getCompany = async (req, res) => {
                 success: false
             })
         }
+        return res.status(200).json({
+            companies,
+            success:true
+        })
+
     } catch (error) {
         console.log(error);
     }
@@ -70,18 +75,18 @@ export const getCompanyById = async (req, res) => {
 
 export const updateCompany = async (req, res) => {
     try {
-        const { company, description, website, location } = req.body;
+        const { name, description, website, location } = req.body;
         const file = req.file;
 
         // cloudinary
 
-        const updateData = { company, description, website, location };
+        const updateData = { name, description, website, location };
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
         if(!company){
             return res.status(404).json({
                 message:"company not found",
-                success:true,
+                success:false,
             })
         }
         return res.status(200).json({
