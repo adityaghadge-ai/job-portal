60a9b6b90d97d157c1eec0e55d5ed7275a7f7a53
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-22 12:42:30 +0530

    updating

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,44 +1,44 @@
-import {Company} from "../models/company.model.js"
-export const registerCompan=async(req,res)=>{
+import { Company } from "../models/company.model.js"
+export const registerCompan = async (req, res) => {
     try {
-         const {companyName}=req.body;
-         if(!companyName){
+        const { companyName } = req.body;
+        if (!companyName) {
             return res.status(400).json({
-                message:"Company Name is required",
-                success:"false"
+                message: "Company Name is required",
+                success: "false"
             })
-         } 
-         let company =await  Company.findOne({name:companyName}) ; 
-         if(company){
+        }
+        let company = await Company.findOne({ name: companyName });
+        if (company) {
             return res.status(400).json({
-                message:"You Cannot register same company",
-                success:"false"
+                message: "You Cannot register same company",
+                success: "false"
 
             })
-         };
-         company=await Company.create({
-            name:companyName,
+        };
+        company = await Company.create({
+            name: companyName,
             userId=req.id
-         })
+        })
 
-         return res.status(201).json({
-            message:"Company registered successfully",
+        return res.status(201).json({
+            message: "Company registered successfully",
             company,
-            success:true
-         })
+            success: true
+        })
     } catch (error) {
-         console.log(error);
+        console.log(error);
     }
 }
 
-export const getCompany=async(req,res)=>{
+export const getCompany = async (req, res) => {
     try {
-        const usedId=req.id;   // logged in user id
-        const companies= await Company.find({userId});
-        if(!companies){
+        const usedId = req.id;   // logged in user id
+        const companies = await Company.find({ userId });
+        if (!companies) {
             return res.status(404).json({
-                message:"Companies not found",
-                success:false
+                message: "Companies not found",
+                success: false
             })
         }
     } catch (error) {
@@ -48,22 +48,47 @@ export const getCompany=async(req,res)=>{
 
 // get company by id 
 
-export const getCompanyById=async(req,res)=>{
+export const getCompanyById = async (req, res) => {
     try {
-       const companyId=req.params.id;
-       const company = await Company.findById(companyId);
-       if(!company){
-          return res.status(404).json({
-                message:"Companies not found",
-                success:false
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if (!company) {
+            return res.status(404).json({
+                message: "Companies not found",
+                success: false
             })
-       }  
-       return res.status(200).json({
-        company,
-        success:true
-       })
+        }
+        return res.status(200).json({
+            company,
+            success: true
+        })
     } catch (error) {
         console.log(error);
 
+    }
+}
+
+export const updateCompany = async (req, res) => {
+    try {
+        const { company, description, website, location } = req.body;
+        const file = req.file;
+
+        // cloudinary
+
+        const updateData = { company, description, website, location };
+
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
+        if(!company){
+            return res.status(404).json({
+                message:"company not found",
+                success:true,
+            })
+        }
+        return res.status(200).json({
+            message:"Company information Updated",
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
