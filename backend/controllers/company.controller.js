74a2d9c6bf0c7d4870582600945690ff80d05e83
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-22 12:32:56 +0530

    company by id

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -48,3 +48,22 @@ export const getCompany=async(req,res)=>{
 
 // get company by id 
 
+export const getCompanyById=async(req,res)=>{
+    try {
+       const companyId=req.params.id;
+       const company = await Company.findById(companyId);
+       if(!company){
+          return res.status(404).json({
+                message:"Companies not found",
+                success:false
+            })
+       }  
+       return res.status(200).json({
+        company,
+        success:true
+       })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
