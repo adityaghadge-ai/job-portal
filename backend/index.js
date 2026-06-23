f56e4861f05c30b2a14a6d57a3ebebc08046e394
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-23 12:10:51 +0530

    `Added job controller and route, updated models and index.js`

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 
+import jobRoute from "./routes/job.route.js";
+
 dotenv.config({});
 const app=express();
 app.use(express.json());
@@ -28,7 +30,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
-
+app.use("/api/v1/job",jobRoute);
 app.get("/", (req, res) => {
     res.send("Server is alive");
 });
--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -9,13 +9,17 @@ const jobSchema=new mongoose.Schema({
         type:String,
         required:true,
     },
-    requirements :{
-        type:String,
-        
-    },
+    requirements: [{
+    type: String,
+    required: true
+}],
     salary:{
         type:Number,
         required:true,
+    },
+    experienceLevel:{
+         type:Number,
+         required:true
     },
     location:{
         type:String,
