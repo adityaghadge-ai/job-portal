3a3a6640ed54e7ba704d70deb1641c15fb86e113
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-23 12:59:52 +0530

    update

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -76,3 +76,64 @@ export const getAppliedJobs=async(req,res)=>{
         console.log(error);
     }
 }
+
+export const getApplicants=async(req,res)=>{
+    try {
+         const jobId=req.params.id;
+         const job=await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            poupulate:{
+                path:'applicant'
+            }
+         });
+   if(!job){
+    return res.status(404).json({
+        message:"Job not found",
+        success:false
+    })
+   };
+
+   return res.status(200).json({
+      job,
+      success:true
+   });
+
+    } catch (error) {
+         console.log(error);
+    }
+}
+
+export const updateStatus=async(req,res)=>{
+    try {
+        const {status}=req.body;
+        const applicationId=req.params.id;
+        if(!status){
+             return res.status(400).json({
+        message:"status is required",
+        success:false
+    }) 
+        };
+        // find the application by applicant id
+       
+        const application =await Application.findOne({_id:applicationId});
+        if(!application){
+            return res.status(404).json({
+                message:"Application Not found",
+                success:false
+            })
+        };
+        // update status
+
+        application.status=status.toLowerCase();
+        await application.save();
+
+        return res.status(200).json({
+            message:"Status updated successfully",
+            success:true
+        })
+          
+    } catch (error) {
+        console.log(error);
+    }
+}
