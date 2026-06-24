36b253b4d6cd457c0f7f306b7999b59582c6f98f
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-24 10:21:20 +0530

    frontend is up and away

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div>
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-[#f83002]">Portal</span>
+        </h1>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
