1459e91d8458ac10288041bbbc11e10038f11e63
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-24 16:06:36 +0530

    login

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState} from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
@@ -7,27 +7,58 @@ import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+ const [input, setInput] = useState({
+  fullname: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  role: "",
+  file: null,
+});
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler=async(e)=>{
+    e.preventDefault();
+    console.log(input);
+  }
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center max-w-7xl mx-auto">
-        <form action="" className="w-1/2 border-gray-200 rounded-md p-4 my-10">
+        <form onSubmit={submitHandler} className="w-1/2 border-gray-200 rounded-md p-4 my-10">
           <h1 className="font-bold text-xl mb-5">Sign Up</h1>
           <div className="my-2">
             <Label>Full Name</Label>
-            <Input type="text" placeholder="Aditya" />
+            <Input type="text" value={input.fullname} name="fullname" 
+            onChange={changeEventHandler}
+            placeholder="Aditya" />
           </div>
           <div className="my-2">
             <Label>Email</Label>
-            <Input type="email" placeholder="Aditya@gmail.com" />
+            <Input type="email"
+            value={input.email} name="email" 
+            onChange={changeEventHandler}
+             placeholder="Aditya@gmail.com" />
           </div>
           <div className="my-2">
             <Label>Phone Number</Label>
-            <Input type="" placeholder="Aditya" />
+            <Input type="text"
+            value={input.phoneNumber} name="phoneNumber" 
+            onChange={changeEventHandler}
+             placeholder="7654456789" />
           </div>
           <div className="my-2">
             <Label>Password</Label>
-            <Input type="password" placeholder="Aditya" />
+            <Input type="password" 
+            value={input.password} name="password" 
+            onChange={changeEventHandler}
+            placeholder="Aditya" />
           </div>
 
           <div className="flex items-center justify-between">
@@ -40,6 +71,8 @@ const Signup = () => {
                   type="radio"
                   name="role"
                   value="student"
+                  checked={input.role==='student'}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r1">Student</Label>
@@ -50,6 +83,8 @@ const Signup = () => {
                   type="radio"
                   name="role"
                   value="recruiter"
+                  checked={input.role==='recruiter'}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r2">Recruiter</Label>
@@ -58,7 +93,10 @@ const Signup = () => {
 
             <div className="flex items-center gap-2">
               <Label>Profile</Label>
-              <Input accept="image/*" type="file" className="cursor-pointer" />
+              <Input accept="image/*"
+               type="file" 
+               onChange={changeFileHandler}
+              className="cursor-pointer" />
             </div>
           </div>
 
@@ -70,7 +108,10 @@ const Signup = () => {
           </Button>
 
           <span className="text-sm">
-            Already have an account ? <Link to="/login" className="text-blue-600">Login</Link>
+            Already have an account ?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
           </span>
         </form>
       </div>
