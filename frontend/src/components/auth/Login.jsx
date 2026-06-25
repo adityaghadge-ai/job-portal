5d0a9521777a650b7b937368a587da5520d81a90
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-25 13:54:39 +0530

    doing some work

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -4,7 +4,8 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
      const [input, setInput] = useState({
@@ -21,9 +22,27 @@ const Login = () => {
     
     
       const submitHandler=async(e)=>{
-        e.preventDefault();
-        console.log(input);
+    e.preventDefault();
+
+    try {
+      const res=await axios.post(`${USER_API_END_POINT}/login`,input,{
+        headers:{
+          "Content-Type":"application/json"
+        },
+        withCredentials:true,
+      });
+      if(res.data.success){
+        navigate("/");
+        toast.success(res.data.message);
       }
+
+    } catch (error) {
+          console.log(error);
+           toast.error(error.response.data.message);
+    }
+
+
+  }
   return (
     <div>
       <Navbar />
