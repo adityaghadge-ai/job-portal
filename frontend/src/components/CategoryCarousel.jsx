fcc7865a55d1f4235fd7b3803193b3b00d09ae02
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-27 00:34:26 +0530

    home page

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+const category =[
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer"
 
+]
 const CategoryCarousel = () => {
   return (
     <div>
-      
+     <Carousel>
+         <CarouselContent>
+
+            <CarouselItem>
+
+                
+            </CarouselItem>
+         </CarouselContent>
+
+     </Carousel>
     </div>
   )
 }
