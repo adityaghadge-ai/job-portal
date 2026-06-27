76e19ce7742603ff1b6e5d1cc0dacabead2944ec
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-27 16:37:46 +0530

    job cards

---
+++ frontend/src/components/LatestJobCards.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LatestJobCards = () => {
+  return (
+    <div className="">
+      <div>
+        <h1>Company Name</h1>
+        <p>India</p>
+      </div>
+
+      <div>
+        <h1>Job Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+          architecto rem corporis a. Minima, harum?
+        </p>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default LatestJobCards;
