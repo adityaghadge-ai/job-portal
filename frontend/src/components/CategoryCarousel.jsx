45f0dec6480e9f5c940a5904c8dea8df55ea6846
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-27 16:08:48 +0530

    some improvement

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
-const category =[
-    "Frontend Developer",
-    "Backend Developer",
-    "Data Science",
-    "Graphic Designer",
-    "FullStack Developer"
-
-]
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { CassetteTapeIcon } from "lucide-react";
+import { Button } from "./ui/button";
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "FullStack Developer",
+];
 const CategoryCarousel = () => {
   return (
     <div>
-     <Carousel>
-         <CarouselContent>
-
-            <CarouselItem>
-
-                
+      <Carousel className="w-full max-w-xl mx-auto my-20 ">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg-basis-1/3 ">
+              <Button variant="outline" className="rounded-full ">
+                {cat}
+              </Button>
             </CarouselItem>
-         </CarouselContent>
-
-     </Carousel>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryCarousel
+export default CategoryCarousel;
