967a3b94779e52a6a9f716e77fba736ac71c3072
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-29 12:24:29 +0530

    :filter card

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { RadioGroup } from './ui/radio-group'
+const filterData=[
+    {
+        filterType:"Location",
+        array:["Delhi NCR ","Pune","Mumbai","Banglore","Hyderabad"]
+    },
+     {
+        filterType:"Industry",
+        array:["Frontend developer","Backend developer","FullStack developer"]
+    },
+     {
+        filterType:"Salary",
+        array:["0-40k","40k-1Lakh","1Lakh-5Lakh"]
+    }
+]
 const FilterCard = () => {
   return (
     <div>
-      filtercard
+  
+  <h1>Filter Jobs</h1>
+  <hr className="mt-3"/>
+  <RadioGroup>
+    {
+        filterData.map((data,index)=(
+            <div></div>
+        ))
+    }
+  </RadioGroup>
     </div>
   )
 }
