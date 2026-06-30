a60604d95933a689f05418df632a13150a4e05c7
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-30 11:25:40 +0530

    new changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Contact, Mail, Pen } from "lucide-react";
+import { AppleIcon, Contact, Mail, Pen } from "lucide-react";
 import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import AppliedJobTable from "./AppliedJobTable";
 
+const skills=["HTML","CSS","JAVASCRIPT","React.js"];
 const Profile = () => {
+  const isResume=true;
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto bg-white border-gray-200 rounded-2xl my-5 p-8">
+      <div className="max-w-4xl mx-auto bg-white border-gray-200 rounded-2xl my-5 p-8">
         <div className="flex justify-between">
           <div className="flex items-center gap-4">
             <Avatar className="h-24 w-24">
@@ -46,15 +50,33 @@ const Profile = () => {
  
   
 </div>
-  <div>
+  <div className="my-5">
     <h1>Skills </h1>
-    {
-      [1,2,3,4].map((item,index)=><Badge key={index}>{item}</Badge>
+    <div className="flex items-center gap-1">
+       {
+     skills.length!=0 ? skills.map((item,index)=><Badge key={index}>{item}</Badge>) : <span>NA</span>
 
-      )
+      
+    }
+    </div>
+   
+  </div>
+
+  <div className="grid w-full max-w-sm items-center gap-1.5">
+   <Label className="text-md font-bold"> Resume</Label>
+    
+    {
+      isResume ? <a target="blank" href="https://youtube.com/@patelmernstack" className="text-blue-500 w-full hover:underline cursor-pointer">Patel Mern Stack</a> : <span> NA </span>
     }
   </div>
+
+ 
       </div>
+       <div className="max-w-4xl mx-auto bg-white rounded-2xl ">
+     <h1>Applied Jobs</h1> 
+     {/* application table */}
+     <AppliedJobTable/>
+  </div>
     </div>
   );
 };
