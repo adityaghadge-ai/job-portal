9e9452fafd33827cb5d35d1a3367867530dd87c5
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-06 08:28:31 +0530

    job description

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Badge } from './ui/badge'
+import { Badge } from './ui/badge';
+import { Button } from "./ui/button";
 
 const JobDescription = () => {
+ const  isApplied=true;
   return (
     <div className="max-w-7xl mx-auto my-10">
-        <h1 className="font-bold text-xl">Title</h1>
+<div className="flex items-center justify-between">
+ <div>
+         <h1 className="font-bold text-xl"> Frontend Developer</h1>
      <div className="flex items-center gap-2 mt-4">
         
             <Badge className={"text-blue-700 font-bold"} variant="ghost">
@@ -18,8 +22,33 @@ const JobDescription = () => {
               4 LPA
             </Badge>
           </div>
+      </div>
+       
+          <Button disabled={isApplied}
+  className={`rounded-lg ${
+    isApplied
+      ? "bg-gray-500 text-white cursor-not-allowed"
+      : "bg-[#7209b7] text-white"
+  }`}
+>
+  {isApplied ? "Already Applied" : "Apply Now !!!"}
+</Button>
+</div>
+<h1 className="border-b-2 border-b-gray-300 font-medium py-4">Job Description</h1>
+<div className="my-4">
+<h1 className="font-bold my-1"> Role : <span className="pl-4 font-normal text-gray-800">Frontend Developer </span></h1>
+<h1 className="font-bold my-1"> Location : <span className="pl-4 font-normal text-gray-800"> Pune </span></h1>
+<h1 className="font-bold my-1"> Description : <span className="pl-4 font-normal text-gray-800">Lorem ipsum dolor sit. </span></h1>
+<h1 className="font-bold my-1"> Experience : <span className="pl-4 font-normal text-gray-800"> 2 years  </span></h1>
+<h1 className="font-bold my-1"> Salary : <span className="pl-4 font-normal text-gray-800">12 LPA </span></h1>
+
+<h1 className="font-bold my-1"> Total Applicants : <span className="pl-4 font-normal text-gray-800"> 4 </span></h1>
+<h1 className="font-bold my-1"> Posted Date : <span className="pl-4 font-normal text-gray-800"> 06-07-2026</span></h1>
+</div>
+    
     </div>
   )
+
 }
 
 export default JobDescription
