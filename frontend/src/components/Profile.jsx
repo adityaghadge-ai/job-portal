71ba18c80ab2072b05aaa24f5d1ec18d90870967
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-07 08:40:49 +0530

    dynamkic

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -8,11 +8,14 @@ import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
 import UpdateProfileDialog from "./UpdateProfileDialog";
+import { useDispatch, useSelector } from "react-redux";
 
-const skills=["HTML","CSS","JAVASCRIPT","React.js"];
+// const skills=["HTML","CSS","JAVASCRIPT","React.js"];
 const isResume=true;
 const Profile = () => {
   const [open,setOpen]=useState(false);
+
+  const {user}=useSelector((store)=>store.auth);
   
   return (
     <div>
@@ -27,10 +30,9 @@ const Profile = () => {
               />
             </Avatar>
             <div>
-              <h1 className="font-medium text-xl">Full Name</h1>
+              <h1 className="font-medium text-xl">{user?.fullname}</h1>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus in fuga veritatis!
+                {user?.profile?.bio ? user?.profile?.bio : "No bio available"}
               </p>
             </div>
           </div>
@@ -43,12 +45,12 @@ const Profile = () => {
 <div className="my-5">
   <div className="flex items-center gap-3 my-2 ">
    <Mail/>
-  <span>sanika@gmail.com</span>
+  <span>{user?.email}</span>
   </div>
   <div className="flex items-center gap-3 my-2">
 <Contact/>
   <span>
-    8010284110
+    {user?.phoneNumber}
   </span>
   </div>
  
@@ -58,7 +60,7 @@ const Profile = () => {
     <h1>Skills </h1>
     <div className="flex items-center gap-1">
        {
-     skills.length!=0 ? skills.map((item,index)=><Badge key={index}>{item}</Badge>) : <span>NA</span>
+     user?.profile?.skills.length!=0 ? user?.profile?.skills.map((item,index)=><Badge key={index}>{item}</Badge>) : <span>NA</span>
 
       
     }
