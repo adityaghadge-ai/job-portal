b1ae75018f8cae8b3668b02d024b6d297e074f3e
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-07 08:32:05 +0530

    update profile

--- frontend/src/components/UpdateProfileDialog.jsx
+++ frontend/src/components/UpdateProfileDialog.jsx
@@ -5,9 +5,68 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { setUser } from '@/redux/authSlice'
 
 const UpdateProfileDialog = ({ open, setOpen }) => {
     const [loading,setLoading]=useState(false);
+    const {user}=useSelector((store)=>store.auth);
+
+    const [input, setInput] = useState({
+        fullname: user?.fullname || "",
+        email: user?.email || "",
+        phoneNumber: user?.phoneNumber || "",
+        bio: user?.profile?.bio || "",
+        skills: user?.profile?.skills?.map(skill => skill) || "",
+        file: user?.profile?.resume || ""
+    });
+    const dispatch=useDispatch();
+    const changeEventHandler=(e)=>{
+setInput({...input,[e.target.name]:e.target.value});
+
+    }
+
+    const fileChangeHandler=(e)=>{
+        const file=e.target.files?.[0];
+        setInput({...input,file});
+    }
+
+   const  submitHandler=async (e)=>{
+    e.preventDefault();
+    const formData=new FormData();
+    formData.append("fullname",input.fullname);
+    formData.append("email",input.email);
+    formData.append("phoneNumber",input.phoneNumber);
+    formData.append("bio",input.bio);
+    formData.append("skills",input.skills);
+    if(input.file){
+        formData.append("file",input.file);
+    }
+
+    try {
+        setLoading(true);
+        const res=await axios.post(`${USER_API_END_POINT}/profile/update`,formData,{
+            headers:{
+                "Content-Type":"multipart/form-data"
+            },
+            withCredentials:true
+        });
+        if(res.data.success){
+            dispatch(setUser(res.data.user));
+            toast.success(res.data.message);
+        }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.response?.data?.message || "Something went wrong");
+    } finally{
+        setLoading(false);
+    }
+    setOpen(false);
+    console.log(input);
+   }
   return (
     <div>
       <Dialog open={open}>
@@ -15,48 +74,48 @@ const UpdateProfileDialog = ({ open, setOpen }) => {
           <DialogHeader>
             <DialogTitle>Update Profile</DialogTitle>
           </DialogHeader>
-          <form>
+          <form onSubmit={submitHandler}>
             <div className="grid gap-4 py-4">
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="name" className="text-right">
                 
                   Name
                 </Label>
-                <Input id="name" name="name" className="col-span-3 border border-gray-600 focus:border-black" />
+                <Input id="name" name="name" type="text" value={input.fullname} onChange={changeEventHandler} className="col-span-3 border border-gray-600 focus:border-black" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="email" className="text-right">
                   
                   Email
                 </Label>
-                <Input id="email" name="email" className="col-span-3 border border-gray-600 focus:border-black" />
+                <Input id="email" name="email" type="email" value={input.email} onChange={changeEventHandler} className="col-span-3 border border-gray-600 focus:border-black" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="number" className="text-right">
                   
                   Number
                 </Label>
-                <Input id="number" name="number" className="col-span-3 border border-gray-600 focus:border-black" />
+                <Input id="number" name="number" value={input.phoneNumber} onChange={changeEventHandler} className="col-span-3 border border-gray-600 focus:border-black" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="bio" className="text-right">
                   Bio
                 </Label>
-                <Input id="bio" name="bio" className="col-span-3 border border-gray-600 focus:border-black" />
+                <Input id="bio" name="bio" value={input.bio}  onChange={changeEventHandler} className="col-span-3 border border-gray-600 focus:border-black" />
               </div>
 
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="skills" className="text-right">
                   Skills
                 </Label>
-                <Input id="skills" name="skills" className="col-span-3 border border-gray-600 focus:border-black" />
+                <Input id="skills" name="skills" value={input.skills}  onChange={changeEventHandler} className="col-span-3 border border-gray-600 focus:border-black" />
               </div>
 
                <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="resume" className="text-right">
                   Resume
                 </Label>
-                <Input id="resume" name="resume" type="file" accept="application/pdf" className="col-span-3" />
+                <Input id="resume" name="resume" type="file" accept="application/pdf" onChange={fileChangeHandler} className="col-span-3" />
               </div>
             </div>
             <DialogFooter>
