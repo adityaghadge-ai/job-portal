f8e18e1c316fed4f6550ea5364ab2b88dca33586
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-13 19:31:52 +0530

    search functionality

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import CompaniesTable from './CompaniesTable'
 import { useNavigate } from 'react-router-dom'
 import useGetAllCompanies from '@/hooks/useGetAllCompanies'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/redux/companySlice'
 
 const Companies = () => {
   useGetAllCompanies();
+  const [input,setInput]=useState("");
+
     const navigate=useNavigate();
+    const dispatch=useDispatch();
+    useEffect(()=>{
+dispatch(setSearchCompanyByText(input));
+    },[input]);
   return (
     <div>
       <Navbar/>
@@ -17,6 +25,7 @@ const Companies = () => {
    <Input
     className="w-fit border border-gray-300 focus-visible:ring-0 focus-visible:border-black"
     placeholder="Filter By Name"
+    onChange={(e)=>setInput(e.target.value)}
   />
 
   <Button onClick={()=>navigate("/admin/companies/create")} className="bg-black text-white hover:bg-gray-800">
--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,72 +1,79 @@
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
-import { Avatar, AvatarImage } from '../ui/avatar'
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
-import { Edit2, MoreHorizontal } from 'lucide-react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Edit2, MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
 
 const CompaniesTable = () => {
+  const { companies,searchCompanyByText } = useSelector((store) => store.company);
+  const [filterCompany,setFilterCompany]=useState(companies);
 
-    const {companies}=useSelector(store=>store.company);
+  useEffect(()=>{
+   const filteredCompany=companies.length>=0 && companies.filter((company)=>{
+    if(!searchCompanyByText){
+        return true;
+    }
+    return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+   });
+   setFilterCompany(filteredCompany);
+  },[companies,searchCompanyByText])
 
-    return (
-        <div>
-            <Table>
-                <TableCaption>
-                    A List of Your recent registered companies
-                </TableCaption>
+  return (
+    <div>
+      <Table>
+        <TableCaption>A List of Your recent registered companies</TableCaption>
 
-                <TableHeader>
-                    <TableRow>
-                        <TableHead> Logo</TableHead>
-                        <TableHead> Name </TableHead>
-                        <TableHead> Date </TableHead>
-                        <TableHead className="text-right"> Action </TableHead>
-                    </TableRow>
-                </TableHeader>
+        <TableHeader>
+          <TableRow>
+            <TableHead> Logo</TableHead>
+            <TableHead> Name </TableHead>
+            <TableHead> Date </TableHead>
+            <TableHead className="text-right"> Action </TableHead>
+          </TableRow>
+        </TableHeader>
 
-                <TableBody>
-                    {
-                        !companies || companies.length <= 0 ? (
-                            <span>Companies Not Found</span>
-                        ) : (
-                            companies?.map((company) => {
-                                return (
+        <TableBody>
+          {filterCompany?.map((company) => (
+            <TableRow key={company._id || company.id || company.name}>
+              <TableCell>
+                <Avatar>
+                  <AvatarImage src={company.logo} />
+                </Avatar>
+              </TableCell>
 
-                                    <div key={company._id}>
-                                        <TableCell>
-                                            <Avatar>
-                                                <AvatarImage src="https://thfvnext.bing.com/th/id/OIP.zPZ5iEhi91xrNkVh7cS0WwHaHa?w=205&h=205&c=7&r=0&o=7&cb=thfvnextfalcon3&pid=1.7&rm=3" />
-                                            </Avatar>
-                                        </TableCell>
+              <TableCell>{company.name}</TableCell>
 
-                                        <TableCell>{company.name}</TableCell>
+              <TableCell>{company.createdAt?.split("T")[0]}</TableCell>
 
-                                        <TableCell>{company.createdAt.split("T")[0]}</TableCell>
+              <TableCell className="text-right cursor-pointer">
+                <Popover>
+                  <PopoverTrigger>
+                    <MoreHorizontal />
+                  </PopoverTrigger>
 
-                                        <TableCell className="text-right cursor-pointer">
-                                            <Popover>
-                                                <PopoverTrigger>
-                                                    <MoreHorizontal />
-                                                </PopoverTrigger>
+                  <PopoverContent className="w-32">
+                    <div className="flex items-center gap-2 w-fit cursor-pointer">
+                      <Edit2 className="w-4" />
+                      <span>Edit</span>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
-                                                <PopoverContent className="w-32">
-                                                    <div className="flex items-center gap-2 w-fit cursor-pointer">
-                                                        <Edit2 className="w-4" />
-                                                        <span>Edit</span>
-                                                    </div>
-                                                </PopoverContent>
-                                            </Popover>
-                                        </TableCell>
-                                    </div>
-                                )
-                            })
-                        )
-                    }
-                </TableBody>
-            </Table>
-        </div>
-    )
-}
-
-export default CompaniesTable
+export default CompaniesTable;
