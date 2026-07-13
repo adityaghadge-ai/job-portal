2ed1615ee99408b1d24676a9f77daeba78fc68e4
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-13 07:56:07 +0530

    company table

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -3,47 +3,70 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Edit2, MoreHorizontal } from 'lucide-react'
+import { useSelector } from 'react-redux'
 
 const CompaniesTable = () => {
-  return (
-    <div>
-      <Table>
-        <TableCaption>
-            A List of Your recent registered companies 
-        </TableCaption>
-        <TableHeader>
-            <TableRow>
-                <TableHead> Logo</TableHead>
-                <TableHead> Name </TableHead>
-                <TableHead> Date </TableHead>
-                <TableHead className="text-right"> Action  </TableHead>
-                
 
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-<TableCell>
-    <Avatar>
-        <AvatarImage src="https://thfvnext.bing.com/th/id/OIP.zPZ5iEhi91xrNkVh7cS0WwHaHa?w=205&h=205&c=7&r=0&o=7&cb=thfvnextfalcon3&pid=1.7&rm=3"/>
-    </Avatar>
-</TableCell>
-<TableCell>Company Name</TableCell>
-<TableCell>11-07-2026</TableCell>
-<TableCell className="text-right cursor-pointer">
-    <Popover>
-        <PopoverTrigger><MoreHorizontal/></PopoverTrigger>
-    <PopoverContent className="w-32">
-<div className="flex items-center gap-2 w-fit cursor-pointer">
-    <Edit2 className="w-4"/>
-    <span>Edit </span>
-</div>
-    </PopoverContent>
-    </Popover>
-</TableCell>
-        </TableBody>
-      </Table>
-    </div>
-  )
+    const {companies}=useSelector(store=>store.company);
+
+    return (
+        <div>
+            <Table>
+                <TableCaption>
+                    A List of Your recent registered companies
+                </TableCaption>
+
+                <TableHeader>
+                    <TableRow>
+                        <TableHead> Logo</TableHead>
+                        <TableHead> Name </TableHead>
+                        <TableHead> Date </TableHead>
+                        <TableHead className="text-right"> Action </TableHead>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    {
+                        !companies || companies.length <= 0 ? (
+                            <span>Companies Not Found</span>
+                        ) : (
+                            companies?.map((company) => {
+                                return (
+
+                                    <div key={company._id}>
+                                        <TableCell>
+                                            <Avatar>
+                                                <AvatarImage src="https://thfvnext.bing.com/th/id/OIP.zPZ5iEhi91xrNkVh7cS0WwHaHa?w=205&h=205&c=7&r=0&o=7&cb=thfvnextfalcon3&pid=1.7&rm=3" />
+                                            </Avatar>
+                                        </TableCell>
+
+                                        <TableCell>{company.name}</TableCell>
+
+                                        <TableCell>{company.createdAt.split("T")[0]}</TableCell>
+
+                                        <TableCell className="text-right cursor-pointer">
+                                            <Popover>
+                                                <PopoverTrigger>
+                                                    <MoreHorizontal />
+                                                </PopoverTrigger>
+
+                                                <PopoverContent className="w-32">
+                                                    <div className="flex items-center gap-2 w-fit cursor-pointer">
+                                                        <Edit2 className="w-4" />
+                                                        <span>Edit</span>
+                                                    </div>
+                                                </PopoverContent>
+                                            </Popover>
+                                        </TableCell>
+                                    </div>
+                                )
+                            })
+                        )
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
 
 export default CompaniesTable
