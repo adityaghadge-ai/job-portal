4928e857de76f3584f54f32d7c672e3cae937c10
Aditya Ghadge <adityaghadge732@gmail.com>
2026-07-16 08:00:56 +0530

    applicants info showing

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { MoreHorizontal } from 'lucide-react';
+import { useSelector } from 'react-redux';
 const shortListingStatus=["Accepted","Rejected"];
 const ApplicantsTable = () => {
+    const {applicants}=useSelector(store=>store.application);
   return (
     <div>
       <Table>
@@ -19,12 +21,19 @@ const ApplicantsTable = () => {
             </TableRow>
               </TableHeader>
             <TableBody>
-                <tr>
-                    <TableCell> Full Name</TableCell>
-                     <TableCell> Email </TableCell>
-                      <TableCell> Contact </TableCell>
-                       <TableCell> Resume</TableCell>
-                        <TableCell> Date</TableCell>
+
+                {
+                    applicants && applicants?.applications?.map((item)=>(
+             <tr key={item._id}>
+                <TableCell>{item?.applicant?.fullname}</TableCell>
+                     <TableCell> {item?.applicant?.email} </TableCell>
+                      <TableCell> {item?.applicant?.phoneNumber} </TableCell>
+                     <TableCell >
+                                    {
+                                        item.applicant?.profile?.resume ? <a className="text-blue-600 cursor-pointer" href={item?.applicant?.profile?.resume} target="_blank" rel="noopener noreferrer">{item?.applicant?.profile?.resumeOriginalName}</a> : <span>NA</span>
+                                    }
+                                </TableCell>
+                        <TableCell> {item?.createdAt.split("T")[0]}</TableCell>
                          <TableCell className="float-right cursor-pointer"> 
                             <Popover>
                                 <PopoverTrigger>
@@ -46,6 +55,11 @@ const ApplicantsTable = () => {
                          </TableCell>
 
                 </tr>
+                    )
+
+                    )
+                }
+                
             </TableBody>
       
       </Table>
